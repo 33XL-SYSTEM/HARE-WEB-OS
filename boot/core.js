20987948cc1cef21d730bf8be56809518e2c08c4
BunnyDev <vr6818999@gmail.com>
2026-09-10 15:59:57 -0300

    Refactor Core class metadata and database initialization

--- boot/core.js
+++ boot/core.js
@@ -1,15 +1,37 @@
 import Database from "../src/js/database.js";
+import path     from "path";
 
 
+// Controla a API do sistema no level 1 (API boot init)
 class Core {
     constructor(root="../") {
-        this.metadata = {currentDataPath: "bunny-os/boot/data", currentBootData: "bootData.db", currentBootLevel: 1};
+        /*
+        dataPath   = path    to    database
+        bootLevel  = system  init  level
+        dataLevel  = system  data  level
+        logLevel   = system  log   level
+        */
+        this.metadata = {
+            current: {
+                dataPath:  path.join(root, "boot", "data", "bootData.db"),
+                dataLevel: 1,
+                bootLevel: 1,
+                logLevel:  1,
+            }
+        };
         this.path = {
             "root": root,
-            "data": this.metadata.currentDataPath,
-            "boot": this.metadata.currentBootData
+            "data": this.metadata.current.dataPath,
         };
-
-        this.db = new Database(this.path.root, this.metadata.currentBootData, "BooTable");
+        try {
+            this.db = new Database(
+                this.path.root,
+                this.metadata.current.dataPath,
+                "SystemTable"
+            );
+        } catch (error) {
+            console.error("ERROR at LEVEL (1)\n" + error);
+            this.db = null;
+        }
     }
 }
