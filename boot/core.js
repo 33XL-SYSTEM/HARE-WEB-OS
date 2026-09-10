6537955d1563e43dc8bec8bba8408f300433ed75
BunnyDev <vr6818999@gmail.com>
2026-09-10 04:55:37 -0300

    Implement Core class with metadata and paths

---
+++ boot/core.js
@@ -0,0 +1,11 @@
+
+
+class Core {
+    constructor() {
+        this.metadata = {currentBootData: "bootData.db", currentBootLevel: 1};
+        this.paths = [
+            {"root": "../path"
+            {"bunny-os/boot/data/": this.metadata.currentBootData}
+        ];
+    }
+}
