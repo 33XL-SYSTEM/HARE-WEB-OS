4db778a4e893974a42e4f806410f1fa705d84c24
BunnyDev <vr6818999@gmail.com>
2026-09-10 05:43:23 -0300

    Refactor Core class constructor and paths structure

--- boot/core.js
+++ boot/core.js
@@ -1,11 +1,15 @@
+import Database from "../src/js/database.js";
 
 
 class Core {
-    constructor() {
-        this.metadata = {currentBootData: "bootData.db", currentBootLevel: 1};
-        this.paths = [
-            {"root": "../path"},
-            {"bunny-os/boot/data/": this.metadata.currentBootData}
-        ];
+    constructor(root="../") {
+        this.metadata = {currentDataPath: "bunny-os/boot/data", currentBootData: "bootData.db", currentBootLevel: 1};
+        this.path = {
+            "root": root,
+            "data": this.metadata.currentDataPath,
+            "boot": this.metadata.currentBootData
+        };
+
+        this.db = new Database(this.path.root, this.metadata.currentBootData, "BooTable");
     }
 }
