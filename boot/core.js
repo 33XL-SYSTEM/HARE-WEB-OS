01c4c6f87a5460553b9925e2f51d8d2c49c20706
BunnyDev <vr6818999@gmail.com>
2026-09-10 04:56:04 -0300

    Fix syntax error in Core class paths array

--- boot/core.js
+++ boot/core.js
@@ -4,7 +4,7 @@ class Core {
     constructor() {
         this.metadata = {currentBootData: "bootData.db", currentBootLevel: 1};
         this.paths = [
-            {"root": "../path"
+            {"root": "../path"},
             {"bunny-os/boot/data/": this.metadata.currentBootData}
         ];
     }
